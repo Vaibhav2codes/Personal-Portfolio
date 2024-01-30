f3b41f4d3f84e89aafb0731e30bd35007f52233f
vaibhav2codes <vaibhav2112001@gmail.com>
2024-01-31 01:35:18 +0530

    banner update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -57,7 +57,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Vaibhav`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Software Developer", "Web Developer"]'><span className="wrap">{text}</span></span></h1>
-                  <p>I'm currently pursuing my Btech Degree in electronics and communication branch in NIT Kurukshetra.I am a fun loving person keen to learn new things every day.I love to act,also love to play sports like cricket,football,basketball.</p>
+                  <p>I'm currently pursuing my Btech Degree in electronics and communication branch in NIT Kurukshetra.I am a fun loving person keen to learn new things every day.I love to act,play sports like cricket,football,basketball.</p>
 
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
